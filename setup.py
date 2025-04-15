5ba3becb208a873734192c499f6a3c36dd99ab8b
Prayesh13 <prayeshgodhani04@gmail.com>
2025-04-15 13:35:36 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A machine learning project that gives prediction of sentiment of the given video comments and gives detailed analysis and made chrome plugin.',
+    author='Prayesh Godhani',
+    license='',
+)
